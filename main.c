cbe8bb1fe1b50d63fa74000803cd066d022be309
vietbui1502 <buivanviet93@gmail.com>
2023-06-16 13:21:47 +0700

    bug fix 2

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 #define DOMAIN_BLACKLIST_FILE "domain_blacklist"
 #define MAX_DOMAIN_LENGTH 128
 
-//static const char *s_listen_on = "ws://10.148.0.3:8081";
-static const char *s_listen_on = "ws://localhost:8081";
+static const char *s_listen_on = "ws://10.148.0.3:8081";
+//static const char *s_listen_on = "ws://localhost:8081";
 static const char *s_web_root = "web_root";
 static struct mg_rpc *s_rpc_head = NULL;
 
